f157df3d0252669abe317896b4bbb75c55c351bb
Ted Zhao <ted.zhao.au@gmail.com>
2026-05-18 17:57:16 +1000

    Use personal site title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ted's Blog",
+    pageTitle: "Ted Zhao",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
